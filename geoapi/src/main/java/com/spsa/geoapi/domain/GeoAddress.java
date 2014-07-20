144372242fcfc1dbb2999c9a6cff647186415151
sarumu1 <sarumu1@searshc.com>
2014-07-19 19:52:16 -0500

    Java Naming convention

--- geoapi/src/main/java/com/spsa/geoapi/domain/GeoAddress.java
+++ geoapi/src/main/java/com/spsa/geoapi/domain/GeoAddress.java
@@ -4,36 +4,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 public class GeoAddress {
-	@JsonProperty("dma_code") private Integer dma_code;
+	@JsonProperty("dma_code") private Integer dmaCode;
 	@JsonProperty("region") private String region;
-	@JsonProperty("continent_code") private String continent_code;
+	@JsonProperty("continent_code") private String continentCode;
 	@JsonProperty("area_code") private Integer area_code;
 	@JsonProperty("isp") private String isp;
-	@JsonProperty("country_code") private String country_code;
+	@JsonProperty("country_code") private String countryCode;
 	@JsonProperty("country") private String country;
 	@JsonProperty("ip") private String ip;
 	@JsonProperty("city") private String city;
 	@JsonProperty("timezone") private String timezone;
-	@JsonProperty("region_code") private Integer region_code;
-	@JsonProperty("country_code3") private String country_code3;
+	@JsonProperty("region_code") private Integer regionCode;
+	@JsonProperty("country_code3") private String countryCode3;
 	@JsonProperty("asn") private String asn;
 	@JsonProperty("longitude") private Double longitude;
 	@JsonProperty("latitude") private Double latitude;
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("GeoAddress [dma_code=");
-		builder.append(dma_code);
+		builder.append("GeoAddress [dmaCode=");
+		builder.append(dmaCode);
 		builder.append(", region=");
 		builder.append(region);
-		builder.append(", continent_code=");
-		builder.append(continent_code);
+		builder.append(", continentCode=");
+		builder.append(continentCode);
 		builder.append(", area_code=");
 		builder.append(area_code);
 		builder.append(", isp=");
 		builder.append(isp);
-		builder.append(", country_code=");
-		builder.append(country_code);
+		builder.append(", countryCode=");
+		builder.append(countryCode);
 		builder.append(", country=");
 		builder.append(country);
 		builder.append(", ip=");
@@ -42,10 +42,10 @@ public class GeoAddress {
 		builder.append(city);
 		builder.append(", timezone=");
 		builder.append(timezone);
-		builder.append(", region_code=");
-		builder.append(region_code);
-		builder.append(", country_code3=");
-		builder.append(country_code3);
+		builder.append(", regionCode=");
+		builder.append(regionCode);
+		builder.append(", countryCode3=");
+		builder.append(countryCode3);
 		builder.append(", asn=");
 		builder.append(asn);
 		builder.append(", longitude=");
@@ -55,4 +55,6 @@ public class GeoAddress {
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
 }
